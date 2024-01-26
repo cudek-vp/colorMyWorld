2e1400973f169424a24b80c6b19fecb053f14099
tomek <tomekcudek@vp.pl>
2024-01-26 22:14:35 +0100

    Pierwszy commit

---
+++ test.py
@@ -0,0 +1,132 @@
+from __future__ import annotations
+
+from typing import Optional
+
+from arcade import Texture
+
+"""
+Starting Template
+
+Once you have learned how to use classes, you can begin your program with this
+template.
+
+If Python and Arcade are installed, this example can be run from the command line with:
+python -m arcade.examples.starting_template
+"""
+import arcade
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "Starting Template"
+
+
+class Object(arcade.Sprite):
+
+    def __init__(self, filename: str = None, scale: float = 1, image_x: float = 0, image_y: float = 0,
+                 image_width: float = 0, image_height: float = 0, center_x: float = 0, center_y: float = 0,
+                 repeat_count_x: int = 1, repeat_count_y: int = 1, flipped_horizontally: bool = False,
+                 flipped_vertically: bool = False, flipped_diagonally: bool = False,
+                 hit_box_algorithm: Optional[str] = "Simple", hit_box_detail: float = 4.5, texture: Texture = None,
+                 angle: float = 0):
+        super().__init__(filename, scale, image_x, image_y, image_width, image_height, center_x, center_y,
+                         repeat_count_x, repeat_count_y, flipped_horizontally, flipped_vertically, flipped_diagonally,
+                         hit_box_algorithm, hit_box_detail, texture, angle)
+
+    def remove_color(self):
+        self.color = arcade.color.WHITE
+
+
+class MyGame(arcade.Window):
+    """
+    Main application class.
+
+    NOTE: Go ahead and delete the methods you don't need.
+    If you do need a method, delete the 'pass' and replace it
+    with your own code. Don't leave 'pass' in this program.
+    """
+
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title)
+
+        arcade.set_background_color(arcade.color.AMAZON)
+
+        self.scene = Optional[arcade.Scene]
+
+        self.setup()
+        # If you have sprite lists, you should create them here,
+        # and set them to None
+
+    def setup(self):
+        """ Set up the game variables. Call to re-start the game. """
+        # Create your sprites and sprite lists here
+        self.scene = arcade.Scene()
+        self.scene.add_sprite_list("objects", True, arcade.SpriteList())
+        sprite = Object(":resources:images/tiles/brickTextureWhite.png")
+        sprite.center_x = 300
+        sprite.center_y = 300
+        sprite.color = arcade.color.RED
+        self.scene.add_sprite("objects", sprite)
+
+    def on_draw(self):
+        """
+        Render the screen.
+        """
+
+        # This command should happen before we start drawing. It will clear
+        # the screen to the background color, and erase what we drew last frame.
+        self.clear()
+        self.scene.draw()
+
+        # Call draw() on all your sprite lists below
+
+    def on_update(self, delta_time):
+        """
+        All the logic to move, and the game logic goes here.
+        Normally, you'll call update() on the sprite lists that
+        need it.
+        """
+        pass
+
+    def on_key_press(self, key, key_modifiers):
+        """
+        Called whenever a key on the keyboard is pressed.
+
+        For a full list of keys, see:
+        https://api.arcade.academy/en/latest/arcade.key.html
+        """
+        pass
+
+    def on_key_release(self, key, key_modifiers):
+        """
+        Called whenever the user lets off a previously pressed key.
+        """
+        pass
+
+    def on_mouse_motion(self, x, y, delta_x, delta_y):
+        """
+        Called whenever the mouse moves.
+        """
+        pass
+
+    def on_mouse_press(self, x, y, button, key_modifiers):
+        """
+        Called when the user presses a mouse button.
+        """
+        pass
+
+    def on_mouse_release(self, x, y, button, key_modifiers):
+        """
+        Called when a user releases a mouse button.
+        """
+        pass
+
+
+def main():
+    """ Main function """
+    game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    game.setup()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
