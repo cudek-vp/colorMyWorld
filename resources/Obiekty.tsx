a6d0995577267669d89c816fc3c1d6cde017f0a1
mfurmane <mfurmane9@gmail.com>
2024-01-26 23:53:34 +0100

    tiled

---
+++ resources/Obiekty.tsx
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Obiekty" tilewidth="1162" tileheight="725" tilecount="8" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="506" height="677" source="credenss.png"/>
+ </tile>
+ <tile id="1">
+  <image width="543" height="677" source="enemy.png"/>
+ </tile>
+ <tile id="2">
+  <image width="302" height="670" source="leftSinkDoor.png"/>
+ </tile>
+ <tile id="3">
+  <image width="511" height="725" source="mainSinkDoor.png"/>
+ </tile>
+ <tile id="4">
+  <image width="79" height="138" source="onion.png"/>
+ </tile>
+ <tile id="5">
+  <image width="292" height="670" source="rightSinkDoor.png"/>
+ </tile>
+ <tile id="6">
+  <image width="281" height="196" source="sink.png"/>
+ </tile>
+ <tile id="7">
+  <image width="1162" height="694" source="table.png"/>
+ </tile>
+</tileset>
