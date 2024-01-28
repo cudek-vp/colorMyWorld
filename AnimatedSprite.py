761d1c2143edd91f5a8d442d8e16211caf170997
Jagoda95 <dziadoszjagoda@gmail.com>
2024-01-28 02:15:10 +0100

    Utwoerzenie klasy Animacji skoku oraz dodanie animacji do spritów Player i Enemy

--- AnimatedSprite.py
+++ AnimatedSprite.py
@@ -11,22 +11,25 @@ class AnimatedSprite(arcade.Sprite):
                  repeat_count_x: int = 1, repeat_count_y: int = 1, flipped_horizontally: bool = False,
                  flipped_vertically: bool = False, flipped_diagonally: bool = False,
                  hit_box_algorithm: Optional[str] = "Simple", hit_box_detail: float = 4.5, texture: Texture = None,
-                 angle: float = 0, distance_to_change: int = 20, main_path = None, walk_sprites: int = 8):
+                 angle: float = 0, distance_to_change: int = 20, jump_path = None, walk_path = None, jump_sprites: int = 8, walk_sprites: int = 8):
         super().__init__(filename, scale, image_x, image_y, image_width, image_height, center_x, center_y,
                          repeat_count_x, repeat_count_y, flipped_horizontally, flipped_vertically, flipped_diagonally,
                          hit_box_algorithm, hit_box_detail, texture, angle)
 
         self.x_moved = 0
-        # How far have we traveled horizontally since changing the texture
+        # How far have we traveled horizontally and vertically since changing the texture
         self.x_odometer = 0
+        self.y_odometer = 0
         # Hit box will be set based on the first image used.
         self.hit_box = self.texture.hit_box_points
         self.character_face_direction = AnimatedSprite.RIGHT_FACING
         # Index of our current texture
         self.cur_texture = 0
         self.distance_to_change = distance_to_change
-        self.main_path = main_path
+        self.walk_path = walk_path
+        self.jump_path = jump_path
         self.walk_sprites = walk_sprites
+        self.jump_sprites = jump_sprites
         self.setup()
 
     def setup(self):
@@ -35,8 +38,14 @@ class AnimatedSprite(arcade.Sprite):
 
         self.walk_textures = []
         for i in range(self.walk_sprites):
-            texture = arcade.load_texture_pair(f"{self.main_path}{i}.png")
+            texture = arcade.load_texture_pair(f"{self.walk_path}{i}.png")
             self.walk_textures.append(texture)
+
+        self.jump_textures = []
+        for i in range(self.jump_sprites):
+            texture = arcade.load_texture_pair(f"{self.jump_path}{i}.png")
+            self.jump_textures.append(texture)
+
 
     def pymunk_moved(self, physics_engine, dx, dy, d_angle):
         super().pymunk_moved(physics_engine, dx, dy, d_angle)
@@ -53,6 +62,7 @@ class AnimatedSprite(arcade.Sprite):
 
         # Add to the odometer how far we've moved
         self.x_odometer += dx
+        self.y_odometer += dy
 
         # Have we moved far enough to change the texture?
         if abs(self.x_odometer) > self.walk_sprites:
@@ -66,8 +76,13 @@ class AnimatedSprite(arcade.Sprite):
                 self.cur_texture = 0
             self.texture = self.walk_textures[self.cur_texture][self.character_face_direction]
 
+        if abs(self.y_odometer) > self.jump_sprites:
 
+            # Reset the odometer
+            self.y_odometer = 0
 
-
-
-
+            # Advance the jumping animation
+            self.cur_texture += 1
+            if self.cur_texture > 7:
+                self.cur_texture = 0
+            self.texture = self.jump_textures[self.cur_texture][self.character_face_direction]
--- Enemy.py
+++ Enemy.py
@@ -6,15 +6,17 @@ from AnimatedSprite import AnimatedSprite
 class Enemy(AnimatedSprite):
     velocity_ratio = 100
 
-    def __init__(self, filename: str = './resources/egg 0.png', scale: float = 0.04, image_x: float = 0,
+    def __init__(self, filename: str = './resources/mr carrot.png', scale: float = 0.04, image_x: float = 0,
                  image_y: float = 0, image_width: float = 0, image_height: float = 0, center_x: float = 0,
                  center_y: float = 0, repeat_count_x: int = 1, repeat_count_y: int = 1,
                  flipped_horizontally: bool = False, flipped_vertically: bool = False, flipped_diagonally: bool = False,
                  hit_box_algorithm: Optional[str] = "Simple", hit_box_detail: float = 4.5, texture: Texture = None,
-                 angle: float = 0, distance_to_change: int = 40, main_path='./resources/egg ', walk_sprites: int = 8):
+                 angle: float = 0, distance_to_change: int = 20, jump_path=None, walk_path=None, jump_sprites: int = 8,
+                 walk_sprites: int = 8):
         super().__init__(filename, scale, image_x, image_y, image_width, image_height, center_x, center_y,
                          repeat_count_x, repeat_count_y, flipped_horizontally, flipped_vertically, flipped_diagonally,
-                         hit_box_algorithm, hit_box_detail, texture, angle, distance_to_change, main_path, walk_sprites)
+                         hit_box_algorithm, hit_box_detail, texture, angle, distance_to_change, jump_path, walk_path,
+                         jump_sprites, walk_sprites)
 
         self.has_color = False
         self.move_time = 1
